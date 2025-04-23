6e8a66af97783aeec069edeb5fa09bf2202aaec8
Venkata Samala <venkata_samala@outlook.com>
2025-04-23 16:04:38 -0700

    ASP.NET Identity

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using QRCoder;
+using RazorPagesPizza;
+using RazorPagesPizza.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection") ?? throw new InvalidOperationException("Connection string 'RazorPagesPizzaAuthConnection' not found.");
+
+
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlite(connectionString));
+
+builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+    options.Conventions.AuthorizePage("/AdminsOnly", "Admin"));
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+builder.Services.AddSingleton(new QRCodeService(new QRCodeGenerator()));
+builder.Services.AddAuthorization(options =>
+    options.AddPolicy("Admin", policy =>
+        policy.RequireAuthenticatedUser()
+            .RequireClaim("IsAdmin", bool.TrueString)));
+
 
 var app = builder.Build();
 
